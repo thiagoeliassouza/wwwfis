616dd8fa5ec475f52c972c949a4b977d3bc98851
thiago souza <thiago.souza@estadao.com>
2018-10-15 22:06:00 -0300

    alt

---
+++ geometria_plana.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+ //$conteudo = json_decode($conteudo);
+?>
+
+<?php include 'master/header_master.php';?>
+
+
+<title>Estudar Física - Geometria Plana</title>
+<meta name="description" content="Estude Geometria Plana, ângulos, retas e semiretas">
+</head>
+
+<body class="hold-transition skin-blue sidebar-mini">
+ 
+    <div class="wrapper">
+    <?php include 'master/header.php';?>
+    <?php include 'master/sidebar.php';?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Geometria Plana
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Geometria Plana</a></li>
+      
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+    
+    <h2>
+      Geometria Plana - Definições
+    </h2>
+    
+    <p>
+        Ângulos: É a reunião de duas semiretas de mesma origem.
+    </p>
+
+    <p>
+        Ângulos opostos pelo vértice: Duas retas concorrentes determinam dois pares de ângulos opostos pelo vétice
+    </p>
+   
+    <p>
+        Ângulos congruentes = iguais
+    </p>
+
+    <p>
+        Bissetriz: é uma semireta que divide o ângulo em dois ângulos congruentes
+    </p>
+   
+    <p>
+        Ângulo reto: É o ângulo que forma 90 graus, ou seja, são semiretas perpendiculares<br>
+        Ângulo agudo: Ãngulo menor que 90 graus (x < 90)<br>
+        Ângulo obtuso: Ãngulo maior que 90 graus (90 < x < 180)<br>
+    </p>
+
+      <p>Medidas em graus<br>
+        1 grau = 1 minuto<br>
+        1 minuto: 1 min = 60''<br>
+        1 segundo: 1 min = 60''<br>
+        Ex:  31grau40min - 10grau45min = 20grau55min
+    </p>
+
+        <p>Medidas em Radianos<br>
+        pi/2, pi, 3pi/2, 2pi
+    </p>
+
+    <p>
+        Ângulos complementares = Dois ângulos são complementares se a soma de suas medidas for 90graus. Um deles é complemento do outro.
+    </p>
+
+       <p>
+        Ângulos suplementares = Dois ângulos são complementares se a soma de suas medidas for 180graus. Um deles é o suplemento do outro.
+    </p>
+
+
+
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+
+  
+<?php include 'master/footer_master.php';?>
+</body>
+</html> 
