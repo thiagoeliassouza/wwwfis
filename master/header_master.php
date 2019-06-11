b91b095bbf67e16c31d696f1d1e81b004c1cc30d
Thiago Elias de Souza <TS104821@grpoesp.com.br>
2019-06-11 14:55:07 -0300

    analytics

--- master/header_master.php
+++ master/header_master.php
@@ -1,13 +1,16 @@
 <head>
-      <!-- Global site tag (gtag.js) - Google Analytics -->
-   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126357284-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+ 
 
-    gtag('config', 'UA-126357284-1');
-  </script>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-141866186-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-141866186-1');
+</script>
+
 
   <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
   <script>
